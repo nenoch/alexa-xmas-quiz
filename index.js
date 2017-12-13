19c7ebfba4f09ced94f8da3645488bf2da00e5b4
Irene Canuti <irene.canuti@gmail.com>
2017-12-13 18:18:49 +0000

    refactoring

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const Alexa = require('alexa-sdk');
-const quiz = require('./quiz.const');
+const quiz = require("./quiz.const");
 
 
-const APP_ID = 'amzn1.ask.skill.10704661-df0b-423e-ad5e-f071318ddc00';
+const APP_ID = "amzn1.ask.skill.10704661-df0b-423e-ad5e-f071318ddc00";
 
-const SKILL_NAME = 'Xmas Quiz';
-const startOutput = `Hi! Let's play. I am going to share 3 christmas facts and you will have to guess whether they're true or false.
-Say "play" when you are ready`;
-const startReprompt = "Hey, are you still there? If you want to play say play.";
-const answerPrompt = " Is it True or False?";
-const helpOutput = `I am here to help! I am going to share 5 christmas facts and you will have to guess whether they're true or false.
-Say "play" if you are ready.`;
+const SKILL_NAME = "Xmas Quiz";
+const startOutput = `Hi! Let's play. I am going to share 3 christmas facts. You will have to guess if they're true or false.
+Say 'play' when you are ready`;
+const startReprompt = "If you want to play say play.";
+const playAgain = " Would you like to start another match? Say play and we'll play again!";
+const wonMessage = `You won!<say-as interpret-as="interjection">Oh Oh Oh</say-as>you're the Christmas Champion.`;
+const lostMessage = "I am afraid you've lost this time, the perfect excuse for some more pudding!";
+const answerPrompt = " Is it true or false?";
+const answerReprompt = "If you'd like me to repeat, say repeat.";
+const helpOutput = `I am here to help! I will share 3 christmas facts for you to guess if they're true or false.
+Say 'play' if you'd like to play.`;
 const helpReprompt = `Remember, you can say play and we'll start playing`;
 const stopOutput = 'Goodbye, Thanks for playing!';
 
@@ -23,43 +27,48 @@ exports.handler = function (event, context, callback) {
 };
 
 var currentQuiz;
+var currentFact;
 var currentAnswer;
 var counter = [];
 
 const handlers = {
     'LaunchRequest': function () {
+        this.event.response.sessionAttributes.hello = "works";
         this.emit(":ask", startOutput, startReprompt);
     },
     'StartQuiz': function () {
-        const quizIndex = Math.floor(Math.random() * quiz.length);
-        currentQuiz = quiz[quizIndex];
-        const speechOutput = currentQuiz.text + answerPrompt;
-        this.emit(':ask', speechOutput);
+        currentQuiz = quiz.slice();
+        getFact.call(this);        
+        const speechOutput = currentFact.text + answerPrompt;
+        this.emit(':ask', speechOutput, answerReprompt);
     },
     'PlayQuiz': function () {
         let reply;
-        if (currentQuiz.answer === currentAnswer) {
-            reply = "That's right, well done!";
+
+        if (currentFact === undefined) {
+            this.emit('LaunchRequest');
+        } else if (currentFact.answer === currentAnswer) {
             counter.push(1);
+            reply = "That's right, well done!";
         } else {
+            counter.push(0);
             reply = "I am afraid that's not correct!";
-            counter.push(0);            
         }
+
         if (counter.length == 3) {
             this.emit('EndQuiz');
         } else {
-            const quizIndex = Math.floor(Math.random() * quiz.length);
-            currentQuiz = quiz[quizIndex];
+            getFact.call(this);
             const next = " Here's another fact: ";
-            const speechOutput = reply + next + currentQuiz.text + answerPrompt;
+            const speechOutput = reply + next + currentFact.text + answerPrompt;
             this.emit(':ask', speechOutput);
         }
     },
     'EndQuiz': function () {
         let score = counter.reduce((a, b) => a + b);
-        const speechOutput = score >= 2 ? "You won! Oh Oh Oh you're the Christmas Champion" : "This time you lose, some more pudding?";
-        const playAgain = " Would you like to start another match? Say play and we'll play again!";
-        this.emit(':ask', speechOutput + playAgain);
+        let endOutput = `This last one was ${currentFact.answer}. Your total score is ${score} out of three`;
+        const speechOutput = score >= 2 ? wonMessage : lostMessage;
+        this.emit(':ask', endOutput + speechOutput + playAgain);
     },
     'TrueIntent': function () {
         currentAnswer = true;
@@ -68,6 +77,10 @@ const handlers = {
     'FalseIntent': function () {
         currentAnswer = false;
         this.emit('PlayQuiz');
+    },
+    'RepeatIntent': function () {
+        const speechOutput = currentFact.text + answerPrompt;
+        this.emit(':ask', speechOutput);
     },
     'AMAZON.HelpIntent': function () {
         this.emit(':ask', helpOutput, helpReprompt);
@@ -78,4 +91,13 @@ const handlers = {
     'AMAZON.StopIntent': function () {
         this.emit(':tell', stopOutput);
     },
+    'SessionEndedRequest': function() {
+        this.emit(':tell', "Sorry, I didn't get that");
+    }
 };
+
+function getFact(){
+    let quizIndex = Math.floor(Math.random() * currentQuiz.length);
+    currentFact = currentQuiz[quizIndex];
+    currentQuiz.splice(quizIndex, 1);
+}
